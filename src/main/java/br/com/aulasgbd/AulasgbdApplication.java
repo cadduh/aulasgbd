a599e015f2c4ea77e8e79f66fb75480044266704
Carlos-edward <carlos.eduardo.brito.dev@gmail.com>
2021-10-28 12:34:30 -0300

    Spring boot + SGBD

---
+++ src/main/java/br/com/aulasgbd/AulasgbdApplication.java
@@ -0,0 +1,13 @@
+package br.com.aulasgbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasgbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasgbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aulasgbd/AulasgbdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aulasgbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasgbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
