824f2869118d1e014552bc73cee7fe899bece9f2
Carlos-edward <carlos.eduardo.brito.dev@gmail.com>
2021-11-04 10:12:12 -0300

    list controller

--- src/main/java/br/com/aulasgbd/controller/UsuarioController.java
+++ src/main/java/br/com/aulasgbd/controller/UsuarioController.java
@@ -1,9 +1,30 @@
 package br.com.aulasgbd.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.aulasgbd.beans.Usuario;
+import br.com.aulasgbd.dao.UsuarioDAO;
 
 @RestController
 public class UsuarioController {
+	
+	private UsuarioDAO dao;
+	
+	@GetMapping("/list")
+	public ResponseEntity<List<Usuario>> getAll(){
+		
+		List<Usuario> list = (List<Usuario>) dao.findAll();
+		
+		if(list.size() == 0) {
+			return ResponseEntity.status(404).build();
+		}
+		return ResponseEntity.ok(list);
+		
+	}
 	
 	
 }
--- src/main/java/br/com/aulasgbd/dao/UsuarioDAO.java
+++ src/main/java/br/com/aulasgbd/dao/UsuarioDAO.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import br.com.aulasgbd.beans.Usuario;
 
-public interface UsuarioDAO extends CrudRepository<Usuario,Integer> {
+public interface UsuarioDAO extends CrudRepository<Usuario, Integer> {
 
 }
