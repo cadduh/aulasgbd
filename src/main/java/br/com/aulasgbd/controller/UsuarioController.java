5861d7f0226ecdb85efd406daf18b3bee5f2421a
Carlos-edward <carlos.eduardo.brito.dev@gmail.com>
2021-11-05 16:58:52 -0300

    CRUD funcionando

--- src/main/java/br/com/aulasgbd/controller/UsuarioController.java
+++ src/main/java/br/com/aulasgbd/controller/UsuarioController.java
@@ -2,7 +2,9 @@ package br.com.aulasgbd.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,19 +12,19 @@ import br.com.aulasgbd.beans.Usuario;
 import br.com.aulasgbd.dao.UsuarioDAO;
 
 @RestController
+@CrossOrigin("*")
 public class UsuarioController {
 	
+	@Autowired
 	private UsuarioDAO dao;
 	
 	@GetMapping("/list")
 	public ResponseEntity<List<Usuario>> getAll(){
-		
-		List<Usuario> list = (List<Usuario>) dao.findAll();
-		
-		if(list.size() == 0) {
+		List<Usuario> lista = (List<Usuario>) dao.findAll();
+		if(lista.size() == 0) {
 			return ResponseEntity.status(404).build();
 		}
-		return ResponseEntity.ok(list);
+		return ResponseEntity.ok(lista);
 		
 	}
 	
